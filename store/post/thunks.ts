05722217b9f6e34645b0b4dbe2c7baebb35c84e6
renetguerra <renetguerra@gmail.com>
2023-04-11 16:22:21 +0200

    update thunks.ts

--- store/post/thunks.ts
+++ store/post/thunks.ts
@@ -1,62 +1,62 @@
 import { postApi } from "@/api";
 import { Post } from "@/interfaces/post";
-import { addPost, deletePostById, filterPost, savingPost, setActivePost, setPosts, setSaving, updatePost } from "./postSlice";
+import {
+  addPost,
+  deletePostById,
+  filterPost,
+  savingPost,
+  setActivePost,
+  setPosts,
+  setSaving,
+  updatePost,
+} from "./postSlice";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { AppDispatch, RootState } from "../store";
 import Cookies from "js-cookie";
 import { getPosts } from "@/helpers/postRequest";
 
 export const startAddPost = (postToCreate: Post) => {
-    return async(dispatch: AppDispatch, getState: () => RootState) => {        
-        dispatch( savingPost() );             
+  return async (dispatch: AppDispatch, getState: () => RootState) => {
+    dispatch(savingPost());
 
-        const response = await postApi.post<Post>('/posts', postToCreate);
-        
-        if (postToCreate.id == 0)
-            postToCreate.id = response.data.id;  
-        
-        //! dispatch
-        dispatch( setActivePost( postToCreate ) );
-        dispatch( addPost( postToCreate ) );
-        
+    const response = await postApi.post<Post>("/posts", postToCreate);
 
+    if (postToCreate.id == 0) postToCreate.id = response.data.id;
+
+    //! dispatch
+    dispatch(setActivePost(postToCreate));
+    dispatch(addPost(postToCreate));
+  };
+};
+
+export const startLoadingPosts = () => {
+  return async (dispatch: AppDispatch, getState: () => RootState) => {
+    const posts = await getPosts();
+
+    const { postListTemp } = getState().post;
+
+    if (postListTemp.length > 0) {
+      dispatch(setPosts(postListTemp));
+    } else {
+      dispatch(setPosts(posts));
     }
-}
+  };
+};
 
-export const startLoadingPosts = () => {    
-    return async( dispatch: AppDispatch, getState: () => RootState ) => {                  
-        const posts = await getPosts();
-                    
-        const postListTemp = localStorage.getItem('postListTemp') != null ? [...JSON.parse(localStorage.getItem('postListTemp'))] : []
-                
-        if (postListTemp.length > 0) {
-            dispatch( setPosts( postListTemp ) );
-        }
-        else {
+export const startSavePost = (postToSave: Post) => {
+  return async (dispatch: AppDispatch, getState: () => RootState) => {
+    dispatch(setSaving());
 
-            dispatch( setPosts( posts ) );
-        }
-    }
-}
+    const { active: post } = getState().post;
 
-export const startSavePost = (postToSave: Post) => {    
-    return async( dispatch: AppDispatch, getState: () => RootState ) => {        
-        dispatch( setSaving() );
-                
-        const { active:post } = getState().post;       
-
-        if (postToSave.id > 0)
-            dispatch( updatePost( postToSave ) );
-        else
-            dispatch( addPost(postToSave) );
-
-    }
-}
+    if (postToSave.id > 0) dispatch(updatePost(postToSave));
+    else dispatch(addPost(postToSave));
+  };
+};
 
 export const startDeletingPost = () => {
-    return async( dispatch: AppDispatch, getState: () => RootState ) => {        
-        const { active: post } = getState().post;        
-        dispatch( deletePostById(post?.id) );
-    }
-}
-
+  return async (dispatch: AppDispatch, getState: () => RootState) => {
+    const { active: post } = getState().post;
+    dispatch(deletePostById(post?.id));
+  };
+};
