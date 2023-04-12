f8ee7783a4a23e385faa733613f03a66beea40dd
renetguerra <renetguerra@gmail.com>
2023-04-12 15:43:44 +0200

    Update store.ts

--- store/store.ts
+++ store/store.ts
@@ -1,14 +1,19 @@
-
-import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit';
-import { postSlice } from './post/postSlice';
-import { postFilterSlice } from './post/postFilterSlice';
+import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
+import { postSlice } from "./post/postSlice";
+import { postFilterSlice } from "./post/postFilterSlice";
 
 export const store = configureStore({
-  reducer: {    
+  reducer: {
     post: postSlice.reducer,
     postFilter: postFilterSlice.reducer,
   },
 });
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
