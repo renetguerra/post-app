1a3408795c059f877bcfe46ad695c08734e407fa
renetguerra <renetguerra@gmail.com>
2023-04-12 15:45:37 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,14 @@
-import { useEffect } from 'react';
-import { NextPage } from 'next';
-import Head from 'next/head';
-import { Inter } from 'next/font/google';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { NextPage } from "next";
+import { Inter } from "next/font/google";
+import { useDispatch, useSelector } from "react-redux";
 
-import { RootState, store } from '@/store';
-import { startLoadingPosts } from '@/store/post/thunks';
-import { Post } from '@/interfaces/post';
-import PostTable from '@/components/post/PostTable';
+import { RootState, store } from "@/store";
+import { startLoadingPosts } from "@/store/post/thunks";
+import { Post } from "@/interfaces/post";
+import PostTable from "@/components/post/PostTable";
 
-import styles from '@/styles/Home.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 interface Props {
   postList: Post[];
@@ -22,7 +19,7 @@ const Home: NextPage<Props> = ({ postList }) => {
   const { active: post, posts } = useSelector((state: RootState) => state.post);
 
   useEffect(() => {
-    dispatch(startLoadingPosts());
+    store.dispatch(startLoadingPosts());
   }, []);
 
   return <PostTable postList={posts} />;
